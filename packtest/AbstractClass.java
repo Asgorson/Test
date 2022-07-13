40c01c902e185181cad42178ad5be9a856e30449
AsgorSon <wokibalf@gmail.com>
2022-07-13 13:01:21 +0300

    Работа с абстрактными классами.

--- packtest/AbstractClass.java
+++ packtest/AbstractClass.java
@@ -3,9 +3,58 @@ package packtest;
 //////////////////////////////////////////////////////
 //         Работа с абстрактными классами           //
 //////////////////////////////////////////////////////
-public class AbstractClass {
-    public static void main(String args[]){
-
+public class AbstractClass{
+      
+    public static void main(String[] args) {
+             
+        Employee sam = new Employee("Sam", "Leman Brothers");
+        sam.display();
+        Client bob = new Client("Bob", "Leman Brothers");
+        bob.display();
     }
+}
+abstract class Person {
+     
+    private String name;
+     
+    public String getName() { return name; }
     
+    public Person(String name){
+     
+        this.name=name;
+    }
+  
+    public abstract void display();// абстрактный метод должен быть определен в потомке
+}
+ 
+class Employee extends Person{
+ 
+    private String bank;
+     
+    public Employee(String name, String company) {
+     
+        super(name);
+        this.bank = company;
+    }
+     
+    public void display(){//определение абстрактного метода ы
+         
+        System.out.printf("Employee Name: %s \t Bank: %s \n", super.getName(), bank);
+    }
+}
+ 
+class Client extends Person
+{
+    private String bank;
+     
+    public Client(String name, String company) {
+     
+        super(name);
+        this.bank = company;
+    }
+     
+    public void display(){
+         
+        System.out.printf("Client Name: %s \t Bank: %s \n", super.getName(), bank);
+    }
 }
