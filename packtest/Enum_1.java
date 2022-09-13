77b1e56e4be228234395fefa9b874bb7ba36cc9a
Asgor <wokibalf@gmail.com>
2022-09-13 20:19:57 +0300

    Перенос всей работы на Linux.

--- packtest/Enum_1.java
+++ packtest/Enum_1.java
@@ -3,17 +3,17 @@ package packtest;
 public class Enum_1 {
     public static void main(String args[]){
 
-        enum Keys{//перечисления можно обявлять где угодно, область видимости, по правилам
+        /*enum Keys{//перечисления можно обявлять где угодно, кроме главвного класса. Область видимости, по правилам
             UP,
             DOWN,
             LEFT,
             RIGHT
-        }
+        }*/
 
         Day Current = Day.Monday; 
         System.out.printf("Day of week: %s\n", Current); 
-        Keys Arrow = Keys.UP; 
-        System.out.printf("Arrow is: %s\n", Arrow);
+        //Keys Arrow = Keys.UP; 
+        //System.out.printf("Arrow is: %s\n", Arrow);
         Answer Solution = Answer.YES; 
         System.out.printf("Decision is: %s\n", Solution);
 
